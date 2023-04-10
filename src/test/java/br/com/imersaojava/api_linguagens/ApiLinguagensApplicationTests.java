3d6d631208a5ecbbf38a088c466f7aebcd8db02e
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-04-10 20:36:13 -0300

    First commit

---
+++ src/test/java/br/com/imersaojava/api_linguagens/ApiLinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imersaojava.api_linguagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLinguagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
